c28f1185f77960ce10e08b36204dd8dec477957d
unknown <cesyrub@gmail.com>
2022-07-09 15:18:58 +0200

    some styles

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -50,15 +50,14 @@ export default function Search() {
     // }
     return (
         <div>
-            <div className="searchForm">
-                <h1>Search</h1>
-                <input type="text" name="search" id="search" ref={inputRef} />
-                <button onClick={search}>Search</button>
+            <div className="searchForm flex flex-row justify-center items-center mt-5">
+                <input type="text" name="search" id="search" className="w-3/12 block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" ref={inputRef} />
+                <button onClick={search} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Search</button>
             </div>
-            <div className="pagination">
+            {/* <div className="pagination">
                 <button onClick={handlePrevPage}>Prev</button>
                 <button onClick={handleNextPage}>Next</button>
-            </div>
+            </div> */}
             <div className="results">
                 {
                     !loading && searchIds !== null && searchIds.map((elem, index) => {
